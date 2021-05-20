a96381330e30de8305249d90eb072ce1d7a3ffcd
GinzaJason <melnikjason@gmail.com>
2021-05-19 22:59:24 -0400

    added class files

---
+++ AstroidSprite.py
@@ -0,0 +1,20 @@
+import pygame
+
+class AstroidSprite(pygame.sprite.Sprite):
+    PW = None
+    PH = None
+
+    def __init__(self, loc, im):
+        super().__init__()
+        self.PW = im.get_width()
+        self.PH = im.get_height()
+        self.image = im
+        self.rect = self.image.get_rect()
+        self.rect.center = loc
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.damage = 1
+
+    def update(self):
+        #goes down
+        pass
---
+++ Resources.py
@@ -0,0 +1,23 @@
+import pygame
+from GlobalVariables import *
+
+resourcesS = 'resources//'
+netim = pygame.image.load(resourcesS + "net.png")
+brickim = pygame.image.load(resourcesS + "spongehead.png")
+background_image_filename = resourcesS + 'spongebobback.png'
+sprite_image_filename = resourcesS + 'jellyfishy.png'
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((640, 480), 0, 32)
+pygame.mixer.music.load(resourcesS + "bubbles.mp3")
+pygame.mixer.music.set_volume(0.25)
+pygame.mixer.music.play()
+pygame.display.set_caption("Keep the jellyfish from reaching the ground!")
+b5blaster = pygame.mixer.Sound(resourcesS + "boing.wav")
+msg = "Jellyfish Wrangler! Click to Play!"
+my_font = pygame.font.SysFont("arial", 32)
+splash = my_font.render(msg, True, (0, 0, 0), (200, 10, 150))
+splashrect = splash.get_rect()
+splashrect.center = (SCREENW / 2, SCREENH / 2)
+background = pygame.image.load(background_image_filename).convert()
+spriteim = pygame.image.load(sprite_image_filename).convert_alpha()
